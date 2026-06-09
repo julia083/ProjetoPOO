09fda867ccab5c84c9e0e7b38e2ef730cb5b076e
julia083 <liaju0101@gmail.com>
2026-06-09 13:55:37 -0300

    adicao de uma tela

--- src/main/java/br/edu/ifpb/ads/foodjava/controller/configuracaoInicial/ConfiguracaoInicialController.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/controller/configuracaoInicial/ConfiguracaoInicialController.java
@@ -1,4 +1,16 @@
 package br.edu.ifpb.ads.foodjava.controller.configuracaoInicial;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class ConfiguracaoInicialController {
+
+    @FXML
+    private Button configurarRestauranteBotton;
+
+    @FXML
+    void configurarORestaurante(ActionEvent event) {
+
+    }
+
 }
