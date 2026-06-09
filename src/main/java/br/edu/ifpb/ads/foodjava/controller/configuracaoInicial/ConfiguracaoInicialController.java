1ff7382420e671622929ca5d2061b2717970d3e9
julia083 <liaju0101@gmail.com>
2026-06-09 15:19:49 -0300

    views do restaurante config inicial

--- src/main/java/br/edu/ifpb/ads/foodjava/controller/configuracaoInicial/ConfiguracaoInicialController.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/controller/configuracaoInicial/ConfiguracaoInicialController.java
@@ -1,7 +1,14 @@
 package br.edu.ifpb.ads.foodjava.controller.configuracaoInicial;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ConfiguracaoInicialController {
 
@@ -9,7 +16,19 @@ public class ConfiguracaoInicialController {
     private Button configurarRestauranteBotton;
 
     @FXML
-    void configurarORestaurante(ActionEvent event) {
+    void configurarORestaurante(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/configuracaoRestaurante.fxml"));
+
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
 
     }
 
--- src/main/java/br/edu/ifpb/ads/foodjava/controller/configuracaoInicial/ConfiguracaoRestauranteController.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/controller/configuracaoInicial/ConfiguracaoRestauranteController.java
@@ -1,4 +1,48 @@
 package br.edu.ifpb.ads.foodjava.controller.configuracaoInicial;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class ConfiguracaoRestauranteController {
+
+    @FXML
+    private TextField categoriaCulinaria;
+
+    @FXML
+    private TextField cnpjRestaurante;
+
+    @FXML
+    private TextField cpfGerente;
+
+    @FXML
+    private TextField emailGerente;
+
+    @FXML
+    private TextField enderecoRestaurante;
+
+    @FXML
+    private TextField nomeFantasia;
+
+    @FXML
+    private TextField nomeGerente;
+
+    @FXML
+    private Button salvarResteuranteBotton;
+
+    @FXML
+    private PasswordField senhaGerente;
+
+    @FXML
+    void salvarCtagoriaCulinaria(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvarORestaurante(ActionEvent event) {
+
+    }
+
 }
