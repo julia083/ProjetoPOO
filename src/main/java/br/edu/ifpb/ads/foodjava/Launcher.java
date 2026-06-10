be912a47a7c9ed13b75ab38310006bb283d7831b
julia083 <liaju0101@gmail.com>
2026-06-09 22:55:14 -0300

    main para teste da tela

--- src/main/java/br/edu/ifpb/ads/foodjava/Launcher.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.ads.foodjava;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(MainApp.class, args);
-    }
-}
