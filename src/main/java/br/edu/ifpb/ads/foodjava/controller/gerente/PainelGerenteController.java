b0d20870443b1370e8749ac5739f0d7df88e5eef
julia083 <liaju0101@gmail.com>
2026-06-10 20:22:12 -0300

    painel do gerente

--- src/main/java/br/edu/ifpb/ads/foodjava/controller/gerente/PainelGerenteController.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/controller/gerente/PainelGerenteController.java
@@ -1,4 +1,66 @@
 package br.edu.ifpb.ads.foodjava.controller.gerente;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
+
 public class PainelGerenteController {
+
+    @FXML
+    private Button avancarStatusBottom;
+
+    @FXML
+    private Button cancelarPedidoBottom;
+
+    @FXML
+    private Button confirmarPedidoBottom;
+
+    @FXML
+    private ComboBox<?> filtroStatus;
+
+    @FXML
+    private GridPane resumoDoDia;
+
+    @FXML
+    private Button sairBottom;
+
+    @FXML
+    private TableView<?> tabelaPedidos;
+
+    @FXML
+    private Button verDetalhesBottom;
+
+    @FXML
+    void avancarStatus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelarPedido(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmarPedido(ActionEvent event) {
+
+    }
+
+    @FXML
+    void listarStatus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verDetalhes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarTelaLogin(ActionEvent event) {
+
+    }
+
 }
