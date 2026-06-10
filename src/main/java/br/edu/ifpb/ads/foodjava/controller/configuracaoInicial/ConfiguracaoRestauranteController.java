08e48f5548d1722982c03bd2aaaf7676c8646cc1
julia083 <liaju0101@gmail.com>
2026-06-10 17:43:10 -0300

    atualizando as views inciais

--- src/main/java/br/edu/ifpb/ads/foodjava/controller/configuracaoInicial/ConfiguracaoRestauranteController.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/controller/configuracaoInicial/ConfiguracaoRestauranteController.java
@@ -5,8 +5,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class ConfiguracaoRestauranteController {
+    @FXML
+    private TextField bairroRestaurante;
+
     @FXML
     private TextField categoriaCulinaria;
 
@@ -20,13 +26,16 @@ public class ConfiguracaoRestauranteController {
     private TextField emailGerente;
 
     @FXML
-    private TextField enderecoRestaurante;
-
-    @FXML
     private TextField nomeFantasia;
 
     @FXML
     private TextField nomeGerente;
+
+    @FXML
+    private TextField numeroRestaurante;
+
+    @FXML
+    private TextField ruaRestaurante;
 
     @FXML
     private Button salvarRestauranteBotton;
@@ -35,8 +44,28 @@ public class ConfiguracaoRestauranteController {
     private PasswordField senhaGerente;
 
     @FXML
+    private TextField telefoneRestaurante;
+
+    @FXML
     void salvarORestaurante(ActionEvent event) {
 
     }
+    @FXML
+    void selecionarLogo(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
 
+        fileChooser.setTitle("Selecionar Logotipo");
+
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter(
+                        "Imagens", "*.png", "*.jpg", "*.jpeg"
+                )
+        );
+
+        File arquivo = fileChooser.showOpenDialog(null);
+
+        if (arquivo != null) {
+            System.out.println(arquivo.getAbsolutePath());
+        }
+    }
 }
