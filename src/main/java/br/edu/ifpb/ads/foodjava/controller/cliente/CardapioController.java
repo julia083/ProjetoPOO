0cd7f9755e171ed86c8fe0fa07bdcaf3fe45d1df
Joel-Meneses <joelwork7744@gmail.com>
2026-06-14 09:30:20 -0300

    criaçao de telas
    criaçao de tela e controllers

--- src/main/java/br/edu/ifpb/ads/foodjava/controller/cliente/CardapioController.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/controller/cliente/CardapioController.java
@@ -1,4 +1,8 @@
 package br.edu.ifpb.ads.foodjava.controller.cliente;
 
+import br.edu.ifpb.ads.foodjava.model.Restaurante;
+
 public class CardapioController {
+    public void setRestaurante(Restaurante restaurante) {
+    }
 }
