c2c3bb43155ad1a193a92f79b9f528318cdfd1d6
julia083 <liaju0101@gmail.com>
2026-06-20 18:29:00 -0300

    removi imports nao usados

--- src/main/java/br/edu/ifpb/ads/foodjava/controller/autenticacao/LoginController.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/controller/autenticacao/LoginController.java
@@ -12,8 +12,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
--- src/main/java/br/edu/ifpb/ads/foodjava/controller/cliente/CarrinhoController.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/controller/cliente/CarrinhoController.java
@@ -16,7 +16,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
