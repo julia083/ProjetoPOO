34516eac5137da0f7546df71df2419092bc42855
julia083 <liaju0101@gmail.com>
2026-06-19 21:06:15 -0300

    cardapiorepository

--- src/main/java/br/edu/ifpb/ads/foodjava/repository/CardapioRepository.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/repository/CardapioRepository.java
@@ -1,4 +1,97 @@
 package br.edu.ifpb.ads.foodjava.repository;
 
-public class CardapioRepository {
+import br.edu.ifpb.ads.foodjava.interfaces.Repositorio;
+import br.edu.ifpb.ads.foodjava.model.ItemCardapio;
+import br.edu.ifpb.ads.foodjava.model.enums.Categoria;
+import br.edu.ifpb.ads.foodjava.util.JsonUtil;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CardapioRepository implements Repositorio<ItemCardapio> {
+
+    private static final String CAMINHO_ARQUIVO = "data/cardapio.json";
+
+    private List<ItemCardapio> itens;
+
+    public CardapioRepository() {
+        this.itens = listarTodos();
+    }
+
+    public void salvar(ItemCardapio item) {
+        itens.add(item);
+        salvarTodos(itens);
+    }
+
+    // Retorna tudo o que está cadastrado
+    public List<ItemCardapio> buscarTodos() {
+        return new ArrayList<>(itens);
+    }
+
+    // Busca um item pelo nome
+    public ItemCardapio buscarPorNome(String nome) {
+        return itens.stream()
+                .filter(item -> item.getNome().equalsIgnoreCase(nome))
+                .findFirst()
+                .orElse(null);
+    }
+
+    // Remove um item pelo nome
+    public void deletar(String nome) {
+        itens.removeIf(item -> item.getNome().equalsIgnoreCase(nome));
+        salvarTodos(itens);
+    }
+
+    // Filtra itens por categoria
+    public List<ItemCardapio> buscarPorCategoria(Categoria categoria) {
+        return itens.stream()
+                .filter(item -> item.getCategoria() == categoria)
+                .collect(Collectors.toList());
+    }
+
+    // Retorna apenas itens disponíveis
+    public List<ItemCardapio> buscarSomenteDisponiveis() {
+        return itens.stream()
+                .filter(ItemCardapio::isDisponivel)
+                .collect(Collectors.toList());
+    }
+
+    // Atualiza um item existente
+    public void atualizar(ItemCardapio itemAtualizado) {
+
+        ItemCardapio itemAntigo = buscarPorNome(itemAtualizado.getNome());
+
+        if (itemAntigo != null) {
+            itemAntigo.setDescricao(itemAtualizado.getDescricao());
+            itemAntigo.setPreco(itemAtualizado.getPreco());
+            itemAntigo.setCategoria(itemAtualizado.getCategoria());
+            itemAntigo.setDisponivel(itemAtualizado.isDisponivel());
+            itemAntigo.setImagemPath(itemAtualizado.getImagemPath());
+        } else {
+            itens.add(itemAtualizado);
+        }
+
+        salvarTodos(itens);
+    }
+
+    @Override
+    public List<ItemCardapio> listarTodos() {
+
+        Type tipoLista = new TypeToken<ArrayList<ItemCardapio>>() {
+        }.getType();
+
+        return JsonUtil.ler(
+                CAMINHO_ARQUIVO,
+                tipoLista,
+                new ArrayList<>()
+        );
+    }
+
+    @Override
+    public void salvarTodos(List<ItemCardapio> lista) {
+        JsonUtil.escrever(CAMINHO_ARQUIVO, lista);
+    }
 }
